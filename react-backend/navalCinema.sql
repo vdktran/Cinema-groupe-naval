f293ad55afb6f7748e95343115c0cad5c832c55b
Alex Tran <info@alex-tran.com>
2019-05-14 15:13:02 +0200

    (development) Added images folder in public, starting the slider component

--- react-backend/navalCinema.sql
+++ react-backend/navalCinema.sql
@@ -1,74 +1,89 @@
-CREATE DATABASE navalDB CHARACTER SET 'utf8';
+CREATE DATABASE navalDB
+CHARACTER
+SET 'utf8';
 
 USE navalDB;
 
 CREATE TABLE films (
     film_id INT PRIMARY KEY AUTO_INCREMENT,
-    titre VARCHAR (250),
+    titre VARCHAR
+(250),
     genre_id INT,
     langue_id INT,
     dimension_id INT,
     date_de_sortie DATE,
-    synopsis VARCHAR (1000),
+    synopsis VARCHAR
+(1000),
     statut_id INT,
-    img_affiche VARCHAR(50),
-    img_background VARCHAR(50)
+    img_affiche VARCHAR
+(50),
+    img_background VARCHAR
+(50)
 )
 ENGINE=INNODB;
 
 INSERT INTO films
-VALUES  (NULL, 'Avengers: End Game', 1, 3, 1, '2019-04-25', "c'est bien", 2, 'afficheAvengers.jpg', 'bakcgroundAvengers.jpg'),
-  (NULL, 'Coeurs Ennenmis', 2, 3, 1, '2019-05-01', "c'est bof", 2, 'afficheCoeursEnnenmis.jpg', 'bakcgroundCoeursEnnenmis.jpg'),
-  (NULL, 'Gloria Bell', 2, 3, 1, '2019-04-25', "c'est pas mal", 2, 'afficheGloriaBell.jpg', 'bakcgroundGloriaBell.jpg'),
-  (NULL, 'Tanguy, Le Retour', 3, 3, 1, '2019-04-10', "c'est super", 2, 'afficheTanguyLeRetour.jpg', 'bakcgroundTanguyLeRetour.jpg'),
-  (NULL, 'Le Parc Des Merveilles', 5, 3, 1, '2019-04-03', "c'est kiffant", 2, 'afficheLeParcDesMerveilles.jpg', 'bakcgroundLeParcDesMerveilles.jpg');
+VALUES
+    (NULL, 'Avengers: End Game', 1, 3, 1, '2019-04-25', "c'est bien", 2, 'afficheAvengers.jpg', 'bakcgroundAvengers.jpg'),
+    (NULL, 'Coeurs Ennenmis', 2, 3, 1, '2019-05-01', "c'est bof", 2, 'afficheCoeursEnnenmis.jpg', 'bakcgroundCoeursEnnenmis.jpg'),
+    (NULL, 'Gloria Bell', 2, 3, 1, '2019-04-25', "c'est pas mal", 2, 'afficheGloriaBell.jpg', 'bakcgroundGloriaBell.jpg'),
+    (NULL, 'Tanguy, Le Retour', 3, 3, 1, '2019-04-10', "c'est super", 2, 'afficheTanguyLeRetour.jpg', 'bakcgroundTanguyLeRetour.jpg'),
+    (NULL, 'Le Parc Des Merveilles', 5, 3, 1, '2019-04-03', "c'est kiffant", 2, 'afficheLeParcDesMerveilles.jpg', 'bakcgroundLeParcDesMerveilles.jpg');
 
 CREATE TABLE genres (
     genre_id INT PRIMARY KEY AUTO_INCREMENT,
-    genre_name VARCHAR (30)
+    genre_name VARCHAR
+(30)
 )
 ENGINE=INNODB;
 
 INSERT INTO genres
-VALUES  (NULL, 'Action'),
-        (NULL, 'Drame'),
-        (NULL, 'Comedie'),
-        (NULL, 'Science Fiction'),
-        (NULL, 'Animation'),
-        (NULL, 'Thriller');
+VALUES
+    (NULL, 'Action'),
+    (NULL, 'Drame'),
+    (NULL, 'Comedie'),
+    (NULL, 'Science Fiction'),
+    (NULL, 'Animation'),
+    (NULL, 'Thriller');
 
 CREATE TABLE dimensions (
     dimension_id INT PRIMARY KEY AUTO_INCREMENT,
-    dimension_name VARCHAR (5)
+    dimension_name VARCHAR
+(5)
 )
 ENGINE=INNODB;
 
-INSERT INTO dimensions 
-VALUES (NULL, '2D'),
-       (NULL, '3D'),
-       (NULL, '2D/3D');
+INSERT INTO dimensions
+VALUES
+    (NULL, '2D'),
+    (NULL, '3D'),
+    (NULL, '2D/3D');
 
 CREATE TABLE langues (
     langue_id INT PRIMARY KEY AUTO_INCREMENT,
-    langues_name VARCHAR (5)
+    langues_name VARCHAR
+(5)
 )
 ENGINE=INNODB;
 
-INSERT INTO langues 
-VALUES (NULL, 'VO'),
-       (NULL, 'VF'),
-       (NULL, 'VO/VF');
+INSERT INTO langues
+VALUES
+    (NULL, 'VO'),
+    (NULL, 'VF'),
+    (NULL, 'VO/VF');
 
 CREATE TABLE statuts (
     statut_id INT PRIMARY KEY AUTO_INCREMENT,
-    statut_name VARCHAR (15)
+    statut_name VARCHAR
+(15)
 )
 ENGINE=INNODB;
 
 INSERT INTO statuts
-VALUES (NULL, 'Retiré'),
-       (NULL, "A l'affiche"),
-       (NULL, 'A venir');
+VALUES
+    (NULL, 'Retiré'),
+    (NULL, "A l'affiche"),
+    (NULL, 'A venir');
 
 CREATE TABLE seances (
     seance_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -76,13 +91,15 @@ CREATE TABLE seances (
     film_id INT,
     date_du_jour DATE,
     horaire TIME,
-    dimension VARCHAR(5),
+    dimension VARCHAR
+(5),
     place_dispo INT
 )
 ENGINE=INNODB;
 
 INSERT INTO seances
-VALUES (NULL, 4, 1, '2019-05-02', '12:45:00', '2D', 300);
+VALUES
+    (NULL, 4, 1, '2019-05-02', '12:45:00', '2D', 300);
 
 CREATE TABLE salles (
     salle_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -90,12 +107,13 @@ CREATE TABLE salles (
 )
 ENGINE=INNODB;
 
-INSERT INTO salles 
-VALUES (NULL, 150),
-       (NULL, 150),
-       (NULL, 150),
-       (NULL, 300),
-       (NULL, 300);
+INSERT INTO salles
+VALUES
+    (NULL, 150),
+    (NULL, 150),
+    (NULL, 150),
+    (NULL, 300),
+    (NULL, 300);
 
 CREATE TABLE reservations (
     reservation_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -107,19 +125,22 @@ CREATE TABLE reservations (
 ENGINE=INNODB;
 
 INSERT INTO reservations
-VALUES (NULL, 1, 1, 3, 2);
+VALUES
+    (NULL, 1, 1, 3, 2);
 
 CREATE TABLE prix (
     prix_id INT PRIMARY KEY AUTO_INCREMENT,
-    nom_du_tarif VARCHAR (30),
+    nom_du_tarif VARCHAR
+(30),
     tarif INT
 )
 ENGINE=INNODB;
 
 INSERT INTO prix
-VALUES (NULL, 'Etudiant', 6.99),
-       (NULL, 'Plein tarif', 12.99),
-       (NULL, 'Moins de 26 ans', 9.50);
+VALUES
+    (NULL, 'Etudiant', 6.99),
+    (NULL, 'Plein tarif', 12.99),
+    (NULL, 'Moins de 26 ans', 9.50);
 
 CREATE TABLE critiques (
     critique_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -132,28 +153,35 @@ ENGINE=INNODB;
 
 CREATE TABLE users (
     users_id INT PRIMARY KEY AUTO_INCREMENT,
-    users_nom VARCHAR (30),
-    users_prenom VARCHAR (50),
-    users_email VARCHAR (30),
+    users_nom VARCHAR
+(30),
+    users_prenom VARCHAR
+(50),
+    users_email VARCHAR
+(30),
     users_naissance DATE,
-    users_telephone VARCHAR(10)
+    users_telephone VARCHAR
+(10)
 )
 ENGINE=INNODB;
 
 INSERT INTO users
-VALUES (NULL, 'Tran', 'Victor', 'Victor@gmail.com', '1970-01-01', '0687873719'),
-       (NULL, 'Choisy', 'Tatiana', 'Tatou@gmail.com', '1950-01-01', '0678980862');
+VALUES
+    (NULL, 'Tran', 'Victor', 'Victor@gmail.com', '1970-01-01', '0687873719'),
+    (NULL, 'Choisy', 'Tatiana', 'Tatou@gmail.com', '1950-01-01', '0678980862');
 
 
 CREATE TABLE newsletter (
     newsletter_id INT PRIMARY KEY AUTO_INCREMENT,
-    newsletter_email VARCHAR (30)
+    newsletter_email VARCHAR
+(30)
 )
 ENGINE=INNODB;
 
 INSERT INTO newsletter
-VALUES (NULL, 'fueijf@gmail.com'),
-       (NULL, 'azertyuiop@hotmail.fr');
+VALUES
+    (NULL, 'fueijf@gmail.com'),
+    (NULL, 'azertyuiop@hotmail.fr');
 
 /* Foreign keys */
 
