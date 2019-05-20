3d0a2a4b0f68d0e8c9b0c1df9e23363d246923b0
Alex Tran <info@alex-tran.com>
2019-05-20 12:39:52 +0200

    slider is finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import ImgGallery from './components js/ImgGallery';
+import SlickSlider from './components js/SliderHero';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {films: []};
-  }
+	constructor(props) {
+		super(props);
+		this.state = { films: [] };
+	}
 
-  componentDidMount() {
-    fetch('/films')
-      .then(res => res.json())
-      .then(films => this.setState({ films }));
-  }
+	componentDidMount() {
+		fetch('/films')
+			.then(res => res.json())
+			.then(films => this.setState({ films }));
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <ImgGallery></ImgGallery>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<SlickSlider></SlickSlider>
+				{console.log(this.state)}
+			</div>
+		);
+	}
 }
 
 export default App;
